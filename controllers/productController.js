6a79c10c0ddfe3b4769ba453de56948fca08539a
Avinash Kumar <Avinash.Kumar@softobiz.com>
2022-11-17 14:29:00 +0530

    new

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,53 @@
+
 const Product = require('../models/product');
+ mongoose  = require("mongoose");
 
-
-
-exports.getAll = function () {
-    return new Promise((resolve) => {
-
-        Product.find({})
-        .then((data) => {
-            console.log(data)
-            resolve(data)
-            res.send(data)
-        }).catch((error) => {
-            resolve("error")
-        })
-    })
+exports.getAll = async (req, res) => {
+    try {
+        const productget = await Product.find({});
+        // res.status(200).render("inventory_list", { productget });
+        res.status(200).send(productget);
+    } catch (e) {
+        res.status(404).send(e.massage);
+    }
 }
 
+
+exports.getById = async(req, res)=>{
+    try{
+        console.log("avinash ",req.params.id);
+        // let id = req.params.id;
+        const product = await Product.where({ "_id": mongoose.Types.ObjectId(req.params.id.trim())});
+        console.log(product);
+        res.send(product);
+    } catch (e) {
+        res.send(e.massage);
+    }
+}
+exports.insertOne=async(req, res)=>{
+    try{
+    var productName= req.body.productName;
+    var description= req.body. description;
+    let categories= mongoose.Types.ObjectId(req.body.id.trim());
+    let price = req.body.price;
+    let noInStock   = req.body.noInStock;
+        const inventorycreate = await Inventory.create({ productName: productName, description: description, categories: categories, price: price, noInStock: noInStock });
+        res.status(200).send(inventorycreate);
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+}
+
+exports.deleteOne= async(req, res)=>{
+    try {
+        
+        const productdelete = await Product.deleteOne({ "_id": mongoose.Types.ObjectId(req.params.id.trim())} );
+        res.status(200).send(productdelete);
+    } catch (e) {
+        res.status(404).send(e.massage);
+    }
+}
+
+exports.update= async(req, res)=>{
+
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,14 @@ var router = express.Router();
 var productController= require('../controllers/productController');
 
 
-
+// product router
 router.get("/getAll",productController.getAll )
+router.get("/:id", productController.getById)
+router.delete("/:id", productController.deleteOne)
+router.post("/insert", productController.insertOne)
+
+
+
+
 
 module.exports = router;
